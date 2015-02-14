29d721c7f7f4e33ad15f26df83f7ac38e29bbdfe
Knut Esten Melandsø Nekså <knutn@mesan.no>
2015-02-14 12:56:58 +0100

    Test

--- src/main/scala/no/mesan/lunsjtavle/config/Configuration.scala
+++ src/main/scala/no/mesan/lunsjtavle/config/Configuration.scala
@@ -12,7 +12,7 @@ trait Configuration {
   val config = ConfigFactory.load()
 
   /** Port to start service on. */
-  lazy val servicePort = Try(config.getInt("service.port")).getOrElse(8080)
+  lazy val servicePort = Try(config.getInt("service.port")).getOrElse(80)
   lazy val serviceHost = Try(config.getString("service.host")).getOrElse("localhost")
 
   /** User name used to access database. */
