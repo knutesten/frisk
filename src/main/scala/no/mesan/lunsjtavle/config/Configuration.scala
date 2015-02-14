4ffe444061c6b1c46e7ebed745656405d4499c8e
Knut Esten Melandsø Nekså <knutn@mesan.no>
2015-02-14 13:37:31 +0100

    Changed host

--- src/main/scala/no/mesan/lunsjtavle/config/Configuration.scala
+++ src/main/scala/no/mesan/lunsjtavle/config/Configuration.scala
@@ -13,8 +13,8 @@ trait Configuration {
 
   /** Port to start service on. */
   lazy val servicePort = Try(config.getInt("service.port")).getOrElse(8080)
-  lazy val serviceHost = Try(config.getString("service.host")).getOrElse("localhost")
-  println(s"Port: $serviceHost")
+  lazy val serviceHost = Try(config.getString("service.host")).getOrElse("0.0.0.0")
+  println(s"Port: $servicePort")
 
   /** User name used to access database. */
   lazy val dbUser = Try(config.getString("db.user")).toOption.orNull
