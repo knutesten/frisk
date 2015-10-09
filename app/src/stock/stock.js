8eca37ae62dece8c5c23c8e1986f2b082daf534e
MikkelSteine <deepskygazer@gmail.com>
2015-10-09 13:39:51 +0200

    GIG stock ticker INSERTED

--- app/src/stock/stock.js
+++ app/src/stock/stock.js
@@ -11,10 +11,11 @@ module.exports = {
       data: { ticker: data }
     }).success(function(data) {
         //console.log(JSON.parse(data));
-        document.getElementById(id).getElementsByClassName("stockName")[0].innerHTML = JSON.parse(data).query.results.quote.Name;
-        document.getElementById(id).getElementsByClassName("stockValue")[0].innerHTML = JSON.parse(data).query.results.quote.Bid;
-        document.getElementById(id).getElementsByClassName("stockChange")[0].innerHTML = JSON.parse(data).query.results.quote.Change;
-        document.getElementById(id).getElementsByClassName("stockPercentChange")[0].innerHTML = JSON.parse(data).query.results.quote.PercentChange;
+        var results = JSON.parse(data).query.results;
+        document.getElementById(id).getElementsByClassName("stockName")[0].innerHTML = results.quote.symbol;
+        document.getElementById(id).getElementsByClassName("stockValue")[0].innerHTML = results.quote.Bid;
+        document.getElementById(id).getElementsByClassName("stockChange")[0].innerHTML = results.quote.Change;
+        document.getElementById(id).getElementsByClassName("stockPercentChange")[0].innerHTML = results.quote.PercentChange;
       });
   }
 };
